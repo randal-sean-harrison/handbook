6a47b3d4973b06fd281392f7ff5407b714fda6cf
Randal Sean Harrison <rharriso@nd.edu>
2017-09-04 10:17:35 -0400

    added to project

---
+++ js/buttons.js
@@ -0,0 +1,305 @@
+$(document).ready(function() {
+
+   $('#checkit-1').click(function() {
+      if ($("#checkit-1").hasClass('conditional')) {
+         $("#checkit-1").removeClass('conditional');
+         $("#checkit-1").addClass('success');
+      } else {
+         $("#checkit-1").removeClass('success');
+         $("#checkit-1").addClass('conditional');
+      }
+   });
+
+   $('#checkit-2').click(function() {
+      if ($("#checkit-2").hasClass('conditional')) {
+         $("#checkit-2").removeClass('conditional');
+         $("#checkit-2").addClass('success');
+      } else {
+         $("#checkit-2").removeClass('success');
+         $("#checkit-2").addClass('conditional');
+      }
+   });
+
+   $('#checkit-3').click(function() {
+      if ($("#checkit-3").hasClass('conditional')) {
+         $("#checkit-3").removeClass('conditional');
+         $("#checkit-3").addClass('success');
+      } else {
+         $("#checkit-3").removeClass('success');
+         $("#checkit-3").addClass('conditional');
+      }
+   });
+
+   $('#checkit-4').click(function() {
+      if ($("#checkit-4").hasClass('conditional')) {
+         $("#checkit-4").removeClass('conditional');
+         $("#checkit-4").addClass('success');
+      } else {
+         $("#checkit-4").removeClass('success');
+         $("#checkit-4").addClass('conditional');
+      }
+   });
+
+   $('#checkit-5').click(function() {
+      if ($("#checkit-5").hasClass('conditional')) {
+         $("#checkit-5").removeClass('conditional');
+         $("#checkit-5").addClass('success');
+      } else {
+         $("#checkit-5").removeClass('success');
+         $("#checkit-5").addClass('conditional');
+      }
+   });
+
+   $('#checkit-6').click(function() {
+      if ($("#checkit-6").hasClass('conditional')) {
+         $("#checkit-6").removeClass('conditional');
+         $("#checkit-6").addClass('success');
+      } else {
+         $("#checkit-6").removeClass('success');
+         $("#checkit-6").addClass('conditional');
+      }
+   });
+
+   $('#checkit-7').click(function() {
+      if ($("#checkit-7").hasClass('conditional')) {
+         $("#checkit-7").removeClass('conditional');
+         $("#checkit-7").addClass('success');
+      } else {
+         $("#checkit-7").removeClass('success');
+         $("#checkit-7").addClass('conditional');
+      }
+   });
+
+   $('#checkit-8').click(function() {
+      if ($("#checkit-8").hasClass('conditional')) {
+         $("#checkit-8").removeClass('conditional');
+         $("#checkit-8").addClass('success');
+      } else {
+         $("#checkit-8").removeClass('success');
+         $("#checkit-8").addClass('conditional');
+      }
+   });
+
+   $('#checkit-9').click(function() {
+      if ($("#checkit-9").hasClass('conditional')) {
+         $("#checkit-9").removeClass('conditional');
+         $("#checkit-9").addClass('success');
+      } else {
+         $("#checkit-9").removeClass('success');
+         $("#checkit-9").addClass('conditional');
+      }
+   });
+
+   $('#checkit-10').click(function() {
+      if ($("#checkit-10").hasClass('conditional')) {
+         $("#checkit-10").removeClass('conditional');
+         $("#checkit-10").addClass('success');
+      } else {
+         $("#checkit-10").removeClass('success');
+         $("#checkit-10").addClass('conditional');
+      }
+   });
+
+
+   $('#checkit-11').click(function() {
+      if ($("#checkit-11").hasClass('conditional')) {
+         $("#checkit-11").removeClass('conditional');
+         $("#checkit-11").addClass('success');
+      } else {
+         $("#checkit-11").removeClass('success');
+         $("#checkit-11").addClass('conditional');
+      }
+   });
+
+   $('#checkit-12').click(function() {
+      if ($("#checkit-12").hasClass('conditional')) {
+         $("#checkit-12").removeClass('conditional');
+         $("#checkit-12").addClass('success');
+      } else {
+         $("#checkit-12").removeClass('success');
+         $("#checkit-12").addClass('conditional');
+      }
+   });
+
+   $('#checkit-13').click(function() {
+      if ($("#checkit-13").hasClass('conditional')) {
+         $("#checkit-13").removeClass('conditional');
+         $("#checkit-13").addClass('success');
+      } else {
+         $("#checkit-13").removeClass('success');
+         $("#checkit-13").addClass('conditional');
+      }
+   });
+
+   $('#checkit-14').click(function() {
+      if ($("#checkit-14").hasClass('conditional')) {
+         $("#checkit-14").removeClass('conditional');
+         $("#checkit-14").addClass('success');
+      } else {
+         $("#checkit-14").removeClass('success');
+         $("#checkit-14").addClass('conditional');
+      }
+   });
+
+   $('#checkit-15').click(function() {
+      if ($("#checkit-15").hasClass('conditional')) {
+         $("#checkit-15").removeClass('conditional');
+         $("#checkit-15").addClass('success');
+      } else {
+         $("#checkit-15").removeClass('success');
+         $("#checkit-15").addClass('conditional');
+      }
+   });
+
+   $('#checkit-16').click(function() {
+      if ($("#checkit-16").hasClass('conditional')) {
+         $("#checkit-16").removeClass('conditional');
+         $("#checkit-16").addClass('success');
+      } else {
+         $("#checkit-16").removeClass('success');
+         $("#checkit-16").addClass('conditional');
+      }
+   });
+
+   $('#checkit-17').click(function() {
+      if ($("#checkit-17").hasClass('conditional')) {
+         $("#checkit-17").removeClass('conditional');
+         $("#checkit-17").addClass('success');
+      } else {
+         $("#checkit-17").removeClass('success');
+         $("#checkit-17").addClass('conditional');
+      }
+   });
+
+   $('#checkit-18').click(function() {
+      if ($("#checkit-18").hasClass('conditional')) {
+         $("#checkit-18").removeClass('conditional');
+         $("#checkit-18").addClass('success');
+      } else {
+         $("#checkit-18").removeClass('success');
+         $("#checkit-18").addClass('conditional');
+      }
+   });
+
+   $('#checkit-19').click(function() {
+      if ($("#checkit-19").hasClass('conditional')) {
+         $("#checkit-19").removeClass('conditional');
+         $("#checkit-19").addClass('success');
+      } else {
+         $("#checkit-19").removeClass('success');
+         $("#checkit-19").addClass('conditional');
+      }
+   });
+
+   $('#checkit-20').click(function() {
+      if ($("#checkit-20").hasClass('conditional')) {
+         $("#checkit-20").removeClass('conditional');
+         $("#checkit-20").addClass('success');
+      } else {
+         $("#checkit-20").removeClass('success');
+         $("#checkit-20").addClass('conditional');
+      }
+   });
+
+
+   $('#checkit-21').click(function() {
+      if ($("#checkit-21").hasClass('conditional')) {
+         $("#checkit-21").removeClass('conditional');
+         $("#checkit-21").addClass('success');
+      } else {
+         $("#checkit-21").removeClass('success');
+         $("#checkit-21").addClass('conditional');
+      }
+   });
+
+   $('#checkit-22').click(function() {
+      if ($("#checkit-22").hasClass('conditional')) {
+         $("#checkit-22").removeClass('conditional');
+         $("#checkit-22").addClass('success');
+      } else {
+         $("#checkit-22").removeClass('success');
+         $("#checkit-22").addClass('conditional');
+      }
+   });
+
+   $('#checkit-23').click(function() {
+      if ($("#checkit-23").hasClass('conditional')) {
+         $("#checkit-23").removeClass('conditional');
+         $("#checkit-23").addClass('success');
+      } else {
+         $("#checkit-23").removeClass('success');
+         $("#checkit-23").addClass('conditional');
+      }
+   });
+
+   $('#checkit-24').click(function() {
+      if ($("#checkit-24").hasClass('conditional')) {
+         $("#checkit-24").removeClass('conditional');
+         $("#checkit-24").addClass('success');
+      } else {
+         $("#checkit-24").removeClass('success');
+         $("#checkit-24").addClass('conditional');
+      }
+   });
+
+   $('#checkit-25').click(function() {
+      if ($("#checkit-25").hasClass('conditional')) {
+         $("#checkit-25").removeClass('conditional');
+         $("#checkit-25").addClass('success');
+      } else {
+         $("#checkit-25").removeClass('success');
+         $("#checkit-25").addClass('conditional');
+      }
+   });
+
+   $('#checkit-26').click(function() {
+      if ($("#checkit-26").hasClass('conditional')) {
+         $("#checkit-26").removeClass('conditional');
+         $("#checkit-26").addClass('success');
+      } else {
+         $("#checkit-26").removeClass('success');
+         $("#checkit-26").addClass('conditional');
+      }
+   });
+
+   $('#checkit-27').click(function() {
+      if ($("#checkit-27").hasClass('conditional')) {
+         $("#checkit-27").removeClass('conditional');
+         $("#checkit-27").addClass('success');
+      } else {
+         $("#checkit-27").removeClass('success');
+         $("#checkit-27").addClass('conditional');
+      }
+   });
+
+   $('#checkit-28').click(function() {
+      if ($("#checkit-28").hasClass('conditional')) {
+         $("#checkit-28").removeClass('conditional');
+         $("#checkit-28").addClass('success');
+      } else {
+         $("#checkit-28").removeClass('success');
+         $("#checkit-28").addClass('conditional');
+      }
+   });
+
+   $('#checkit-29').click(function() {
+      if ($("#checkit-29").hasClass('conditional')) {
+         $("#checkit-29").removeClass('conditional');
+         $("#checkit-29").addClass('success');
+      } else {
+         $("#checkit-29").removeClass('success');
+         $("#checkit-29").addClass('conditional');
+      }
+   });
+
+   $('#checkit-30').click(function() {
+      if ($("#checkit-30").hasClass('conditional')) {
+         $("#checkit-30").removeClass('conditional');
+         $("#checkit-30").addClass('success');
+      } else {
+         $("#checkit-30").removeClass('success');
+         $("#checkit-30").addClass('conditional');
+      }
+   });
+
+});
